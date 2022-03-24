0b1b18e60c8033e54bf8f9b88f51fa4903436a85
Chris Chicas <christianchicas7@gmail.com>
2022-03-24 16:51:04 -0400

    react-router-dom implemented

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import { useState, useRef } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Gallery from './components/Gallery'
 import SearchBar from './components/SearchBar'
 import { DataContext } from './components/context/DataContext'
 import { SearchContext } from './components/context/SearchContext'
+import ArtistView from './components/ArtistView'
+import AlbumView from './components/AlbumView'
+import { Fragment } from 'react/cjs/react.production.min'
 
 function App() {
 	let [message, setMessage] = useState('Search for Music!')
@@ -28,16 +32,26 @@ function App() {
 
 	return (
 		<div>
-			<SearchContext.Provider value={{
-				term: searchInput,
-				handleSearch: handleSearch
-			}}>
-				<SearchBar/>	
-			</SearchContext.Provider>
 			{message}
-			<DataContext.Provider value={data}>
-				<Gallery/>	
-			</DataContext.Provider>
+			<Router>
+				<Routes>
+					<Route path="/" element={
+						<Fragment>
+							<SearchContext.Provider value={{
+								term: searchInput,
+								handleSearch: handleSearch
+							}}>
+								<SearchBar/>	
+							</SearchContext.Provider>
+							<DataContext.Provider value={data}>
+								<Gallery/>	
+							</DataContext.Provider>
+						</Fragment>
+					}/>
+					<Route path="/artist/:id" element={<ArtistView/>}/>
+					<Route path="/album/:id" element={<AlbumView/>}/>
+				</Routes>
+			</Router>
 		</div>
   	);
 }
